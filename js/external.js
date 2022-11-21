1a5ee38637bd90d42013b108f349c261b769f5d5
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2022-11-21 13:38:13 -0600

    Moved from old repository

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello from external JavaScript');
+alert('Welcome to my Website!');
+
+var userInput = prompt('What is your favorite color?');
+console.log('users favorite color response was ' + userInput);
+alert('Great my favorite color is also ' + userInput);
