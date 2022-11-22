15fbe273ef6cdcbe274bc15f211850e5678b824d
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2022-11-22 01:06:42 -0600

    answers 1-3

--- function.js
+++ function.js
@@ -20,19 +20,24 @@ function isTwo(number) {
 
 isTwo(random);
 
+// let userBillTotal = prompt('How much was your bill?');
+// console.log('total bill: ' + userBillTotal);
+// let userBillTip = prompt ('What would you like to tip?');
+// console.log('tip: ' + userBillTotal.toString() * userBillTip.toString());
+// let tip = userBillTotal * userBillTip;
+// alert('Thank you for the ' + tip + " tip!");
 
-let userBill = prompt ('How much was your bill?');
-letprompt ('How much would you like to tip?');
+//function calculateTip(totalBill, tipPercentage){
+   // let total = (totalBill * tipPercentage);
+    //console.log(total);
+//}
+//calculateTip();
 
-userBill = calculateTip()
 
-function calculateTip(totalBill, tipPercentage){
-    let total = (totalBill * tipPercentage);
-    console.log(total)
+function applyDiscount(){
+    let price = 100;
+        console.log(price);
+    let discount = .2;
+        console.log(discount);
 }
-
-
-
-
-
-
+applyDiscount();
